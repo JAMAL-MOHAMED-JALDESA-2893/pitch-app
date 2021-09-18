bb49a82c53e26fd2a611660fcf529e6a249ed43e
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-18 10:46:56 +0300

    add class pitch ,update file, pitchform and comment form classes in forms.py file

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,12 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
 
+class ReviewForm(FlaskForm):
     title = StringField('Review title',validators=[Required()])
     review = TextAreaField('Movie review')
     submit = SubmitField('Submit')
-
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+class PitchForm(FlaskForm):
+  title = StringField('Pitch title')
+  category = SelectField("Choose Category",choices=[('Brand','Personal brand'),('Product','Product'),('Project','Project'),('Investor','Investor')])
+  pitch = TextAreaField('Your Pitch',validators=[Required()])
+  submit = SubmitField('Submit')
+class CommentForm(FlaskForm):
+  comment = TextAreaField('Your Comment',validators=[Required()])
+  submit = SubmitField('Submit')
