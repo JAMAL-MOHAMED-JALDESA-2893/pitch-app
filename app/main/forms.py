3862c6eab00658e734259f64499fc905788b0050
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-20 15:15:11 +0300

    add some fields to forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,30 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
-
-from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review')
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[Required()])
+    text = TextAreaField('Text',validators=[Required()])
+    category = SelectField('Type',choices=[('motivation','Motivation pitch'),('love','Love pitch'),('wisdom','Wisdom pitch')],validators=[Required()])
     submit = SubmitField('Submit')
-
-
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Bio.',validators = [Required()])
     submit = SubmitField('Submit')
 
-
-class PitchForm(FlaskForm):
-  title = StringField('Pitch title')
-  category = SelectField("Choose Category",choices=[('Brand','Personal brand'),('Product','Product'),('Project','Project'),('Investor','Investor')])
-  pitch = TextAreaField('Your Pitch',validators=[Required()])
-  submit = SubmitField('Submit')
-
-  
 class CommentForm(FlaskForm):
-  comment = TextAreaField('Your Comment',validators=[Required()])
-  submit = SubmitField('Submit')
+    text = TextAreaField('Leave a comment:',validators=[Required()])
+    submit = SubmitField('Submit')
