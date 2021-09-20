c295c0d57f3a7ac7340188b57b601d9e4fbf7cce
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-20 15:18:01 +0300

    add changes to auth views route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,10 @@
-from flask import render_template,redirect,url_for,flash,request
+from flask import render_template,redirect,url_for, flash,request
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User
-from .forms import RegistrationForm,LoginForm
+from .forms import LoginForm,RegistrationForm
 from .. import db
-from flask_login import login_user,login_required,logout_user
 from ..email import mail_message
-
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -18,30 +17,26 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Best Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
-    
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to the Pitches","email/welcome_user", user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    flash('You are logged out')
     return redirect(url_for("main.index"))
-
-
-@auth.route('/register', methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        db.session.add(user)
-        db.session.commit()
-
-        mail_message("Welcome to watchlist","email/welcome_user",user.email,user=user)
-
-        return redirect(url_for('auth.login'))
-
-    title = "New Account"
-
-    return render_template('auth/register.html',registration_form = form,title = title)    
